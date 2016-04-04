12348ee66024ab25839ec3c94ffa563d07a55e96
jt222ii <jt222ii@student.lnu.se>
2016-04-04 21:46:51 +0200

    New arrow sprite. Physics for the arrow. Arrow sticks to wall. You can now shoot arrows. Arrows can be deflected by other arrows.

--- MultiplayerGame/Assets/ArrowScript.cs
+++ MultiplayerGame/Assets/ArrowScript.cs
@@ -3,13 +3,36 @@ using System.Collections;
 
 public class ArrowScript : MonoBehaviour {
     public float speed;
+    public Vector2 minSpeedUntilHarmless;
+    public Collider2D collision, hitBox;
+
+    private bool harmless = false;
+    private Rigidbody2D rigidBody;
+
 	// Use this for initialization
 	void Start () {
-        GetComponent<Rigidbody2D>().velocity = transform.right * speed; 
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.velocity = transform.right * speed; 
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-	}
+
+    // Update is called once per frame
+    void Update() {
+        Vector2 moveDirection = rigidBody.velocity;
+        if (moveDirection != Vector2.zero && !harmless)
+        {
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Wall")
+        {
+            rigidBody.velocity = Vector2.zero;
+            rigidBody.gravityScale = 0;
+            Destroy(collision);
+            Destroy(hitBox);
+            harmless = true;
+        }
+    }
 }
---
+++ MultiplayerGame/Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public GameObject projectile;
+    public Transform projectileSpawn;
+    public float fireRate;
+
+
+    private float nextShot;
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Fire1") && Time.time > nextShot)
+        {
+            nextShot = Time.time + (1 / fireRate);          
+            Instantiate(projectile, projectileSpawn.position, projectileSpawn.rotation);
+        }
+    }
+}
