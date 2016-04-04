a5d1fb756aee9c79f7d304090ed3200328c783ff
jt222ii <jt222ii@student.lnu.se>
2016-04-04 18:57:36 +0200

    Started ArrowScript and arrow

---
+++ MultiplayerGame/Assets/ArrowScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowScript : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = transform.right * speed; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
