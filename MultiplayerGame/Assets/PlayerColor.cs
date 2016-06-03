95b4decca9e4690b718841bd2d22ea58d88b3309
jt222ii <jt222ii@student.lnu.se>
2016-06-03 17:30:48 +0200

    Enemy player is now red so it is easier to differentiate the players from each other.

---
+++ MultiplayerGame/Assets/PlayerColor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerColor : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (isLocalPlayer) {
+			Debug.Log ("hejhej");
+			var hej = GetComponentInChildren<SpriteRenderer> ();
+			Debug.Log (hej);
+			GetComponentInChildren<SpriteRenderer> ().color = Color.white;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
